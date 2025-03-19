d2b0307b5980a7fd86e365bad481c7cd6a9e264b
Nazan Korkmaz <nazzankorkmazz@gmail.com>
2025-03-19 14:49:03 +0300

    model guncellendi.

--- book_api/models.py
+++ book_api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=20)
     page_number = models.IntegerField()
     publish_data = models.DateField()
     stock = models.IntegerField()
